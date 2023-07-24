0091b2f82fa9c6f30410ad86c7125442cec6911e
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2023-07-24 19:51:39 +0330

    Refactor dashboard to its base form

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,13 +1,7 @@
 import { Stack } from "@mui/material";
-import { Body, Sidebar } from "./components";
 
 const Dashboard = () => {
-  return (
-    <Stack direction="row" sx={{ height: "100vh", width: "100vw" }}>
-      <Sidebar />
-      <Body />
-    </Stack>
-  );
+  return <Stack>HERE</Stack>;
 };
 
 export default Dashboard;
--- src/views/Dashboard/index.ts
+++ src/views/Dashboard/index.ts
@@ -1,3 +1,2 @@
 export { default } from "./Dashboard";
 export { default as loader } from "./loader";
-export { default as ErrorPage } from "./ErrorPage";
