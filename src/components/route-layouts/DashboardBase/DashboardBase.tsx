d8b2a030258408525e136df8449bbc7f10eb933d
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2023-07-25 11:38:59 +0330

    refactor DashboardBase

--- src/components/route-layouts/DashboardBase/DashboardBase.tsx
+++ src/components/route-layouts/DashboardBase/DashboardBase.tsx
@@ -1,18 +1,22 @@
+import { Box, Stack } from "@mui/material";
 import { useOutlet } from "react-router-dom";
 import { Header } from "./components";
 import { Navbar } from "./components/Sidebar/components";
+import * as sx from "./styles";
 
 const DashboardBase = () => {
   const outlet = useOutlet();
 
   return (
-    <>
-      <Header />
-      <main>
-        <Navbar />
-        <section>{outlet}</section>
-      </main>
-    </>
+    <Box sx={sx.root}>
+      <Header sx={sx.header} />
+      <Stack component="main" direction="row" sx={sx.main}>
+        <Navbar sx={sx.navbar} />
+        <Box component="section" sx={sx.content}>
+          {outlet}
+        </Box>
+      </Stack>
+    </Box>
   );
 };
 
