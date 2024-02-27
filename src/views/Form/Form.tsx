cdf032a83fd46b9855f54b1c42b23fc360c7210c
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-02-27 19:09:53 +0330

    add defaultValues to form.

--- src/views/Form/Form.tsx
+++ src/views/Form/Form.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Box, Stack } from "@mui/material";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { useLoaderData } from "react-router-dom";
-import { SchemaStateManagerProvider } from "services";
+import { SchemaProvider, SchemaStateManagerProvider } from "services";
 import { SchemaForm } from "services/schema/components";
+import { type SchemaID } from "services/schema/types";
 import { BackButton, Footer, Header, SubmitButton } from "./components";
 import { type LoaderData } from "./loader";
 import * as sx from "./styles";
@@ -11,7 +13,49 @@ import { getSchemaPages } from "./utils";
 
 const Form = () => {
   const schema = useLoaderData() as LoaderData;
-  const form = useForm({ mode: "all" });
+
+  const formDefaultValues = React.useMemo(() => {
+    const { widgets } = schema.definitions;
+
+    return Object.keys(widgets).reduce<Record<SchemaID, unknown>>(
+      (defaultValues, widgetId) => {
+        const widget = widgets[widgetId]!;
+
+        if (widget.type !== "field") return defaultValues;
+
+        let fieldDefaultValue: unknown;
+        const { type, properties } = widget.properties;
+
+        switch (type) {
+          case "boolean": {
+            fieldDefaultValue = properties.defaultChecked;
+            break;
+          }
+
+          case "choice":
+          case "number":
+          case "string": {
+            fieldDefaultValue = properties.defaultValue;
+            break;
+          }
+
+          default:
+            return defaultValues;
+        }
+
+        defaultValues[widget.id] = fieldDefaultValue;
+
+        return defaultValues;
+      },
+      {},
+    );
+  }, [schema]);
+
+  const form = useForm({
+    mode: "all",
+    defaultValues: formDefaultValues as object,
+    // shouldUnregister: true,
+  });
 
   const btnRef = React.useRef<HTMLButtonElement>(null);
 
@@ -29,38 +73,40 @@ const Form = () => {
     <>
       <Header />
       <Stack direction="column" sx={sx.wrapper}>
-        <SchemaStateManagerProvider form={form} schemaPages={pages}>
-          <Box sx={sx.main} component="main">
-            <SchemaForm
-              form={form}
-              schema={schema}
-              submitButton={
-                <button
-                  ref={btnRef}
-                  type="submit"
-                  style={{ display: "none" }}
+        <SchemaProvider schema={schema}>
+          <FormProvider {...form}>
+            <SchemaStateManagerProvider>
+              <Box sx={sx.main} component="main">
+                <SchemaForm
+                  submitButton={
+                    <button
+                      ref={btnRef}
+                      type="submit"
+                      style={{ display: "none" }}
+                    />
+                  }
                 />
-              }
-            />
-          </Box>
-          <Footer
-            sx={sx.footer}
-            submitButton={
-              <SubmitButton
-                sx={sx.submitButton}
-                text={schema.submitButtonText}
-                onClick={handleSubmitClick}
+              </Box>
+              <Footer
+                sx={sx.footer}
+                submitButton={
+                  <SubmitButton
+                    sx={sx.submitButton}
+                    text={schema.submitButtonText}
+                    onClick={handleSubmitClick}
+                  />
+                }
+                backButton={
+                  <BackButton
+                    onClick={handleBackClick}
+                    sx={sx.backButton}
+                    schemaPages={pages}
+                  />
+                }
               />
-            }
-            backButton={
-              <BackButton
-                onClick={handleBackClick}
-                sx={sx.backButton}
-                schemaPages={pages}
-              />
-            }
-          />
-        </SchemaStateManagerProvider>
+            </SchemaStateManagerProvider>
+          </FormProvider>
+        </SchemaProvider>
       </Stack>
     </>
   );
