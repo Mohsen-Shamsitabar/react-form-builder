dce06e43c1d39bc19d1cdea2af9b2eb9447f7653
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-07-18 18:27:25 +0330

    add types for effect default values splitting.

--- src/views/CreateForm/components/EditModal/types.ts
+++ src/views/CreateForm/components/EditModal/types.ts
@@ -1,1 +1,7 @@
 export type TabState = "effects" | "settings";
+
+type UUID = string;
+export type FnID = `FN_${UUID}` | `ROOTFN_${UUID}`;
+export type EffectID = `EFFECT_${UUID}`;
+export type EffectFieldNames = "effectType" | "actionType" | "actionPayload";
+export type FnFieldNames = "operator" | "fieldId" | "value" | "fn1" | "fn2";
