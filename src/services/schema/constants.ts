713d9f2e4fb62edb1d871e641a5c99038e1a5006
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-07-31 15:36:24 +0330

    add new constants

--- src/services/schema/constants.ts
+++ src/services/schema/constants.ts
@@ -1,3 +1,5 @@
+import { type ChoiceOption } from "./types";
+
 export enum ComparisonTypes {
   EQ = "EQ",
   NEQ = "NEQ",
@@ -21,3 +23,21 @@ export enum FieldAction {
 export enum PageAction {
   GO_TO_PAGE = "GO_TO_PAGE",
 }
+
+export const FIELD_WIDGET_TYPE_OPTIONS: ChoiceOption[] = [
+  { label: "String", value: "string" },
+  { label: "Number", value: "number" },
+  { label: "Choice", value: "choice" },
+  { label: "Boolean", value: "boolean" },
+];
+
+export const UI_WIDGET_TYPE_OPTIONS: ChoiceOption[] = [
+  { label: "Text", value: "text" },
+  { label: "Link", value: "link" },
+  { label: "Divider", value: "divider" },
+];
+
+export const WIDGET_TYPE_OPTIONS: ChoiceOption[] = [
+  { label: "Field", value: "field" },
+  { label: "UI", value: "ui" },
+];
