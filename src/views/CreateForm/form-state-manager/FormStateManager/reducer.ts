a68f505f80b5f0d982de14a47cb4bdaf2328068b
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-08-10 22:48:50 +0330

    connect `FormStateManager` to `DeleteModal`

--- src/views/CreateForm/form-state-manager/FormStateManager/reducer.ts
+++ src/views/CreateForm/form-state-manager/FormStateManager/reducer.ts
@@ -1,6 +1,11 @@
 import type * as React from "react";
+import { type SchemaID } from "services/schema/types";
 import { filterObject } from "utils";
-import { type CreateFormData } from "../../types";
+import {
+  type CreateFormData,
+  type PageNode,
+  type WidgetNode,
+} from "../../types";
 import { ActionType, type Action } from "./actions";
 
 export type State = CreateFormData;
@@ -43,22 +48,62 @@ const reducer: React.Reducer<State, Action> = (state, action) => {
     case ActionType.REMOVE_PAGE: {
       const { pageId } = action.payload;
 
+      const deletedPage = state.pages.byId[pageId] as PageNode;
+
+      const remainingPages: SchemaID[] = state.pages.allIds.filter(
+        id => id !== deletedPage.id,
+      );
+
+      const remainingWidgets: SchemaID[] = state.widgets.allIds.filter(
+        id => !deletedPage.widgets.includes(id),
+      );
+
+      const remainingEffects: SchemaID[] = state.effects.allIds.filter(
+        id => !deletedPage.effects?.includes(id),
+      );
+
       return {
-        ...state,
         pages: {
-          byId: filterObject(state.pages.byId, [pageId]),
-          allIds: state.pages.allIds.filter(id => id !== pageId),
+          byId: filterObject(state.pages.byId, remainingPages),
+          allIds: remainingPages,
         },
+        widgets: {
+          byId: filterObject(state.widgets.byId, remainingWidgets),
+          allIds: remainingWidgets,
+        },
+        effects: !deletedPage.effects
+          ? state.effects
+          : {
+              byId: filterObject(state.effects.byId, remainingEffects),
+              allIds: remainingEffects,
+            },
       };
     }
     case ActionType.REMOVE_WIDGET: {
       const { widgetId } = action.payload;
 
+      const deletedWidget = state.widgets.byId[widgetId] as WidgetNode;
+      const effectedPage = state.pages.byId[deletedWidget.pageId] as PageNode;
+
+      const remainingWidgets: SchemaID[] = state.widgets.allIds.filter(
+        id => id !== deletedWidget.id,
+      );
+
+      const remainingPageWidgets = effectedPage.widgets.filter(
+        id => id !== widgetId,
+      );
+
+      effectedPage.widgets = remainingPageWidgets;
+
       return {
         ...state,
+        pages: {
+          ...state.pages,
+          byId: { ...state.pages.byId, [effectedPage.id]: effectedPage },
+        },
         widgets: {
-          byId: filterObject(state.widgets.byId, [widgetId]),
-          allIds: state.widgets.allIds.filter(id => id !== widgetId),
+          byId: filterObject(state.widgets.byId, remainingWidgets),
+          allIds: remainingWidgets,
         },
       };
     }
