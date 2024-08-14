8680084ba3a85e70394ba56af7e6e80a604803af
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-08-14 16:05:19 +0330

    move type to `services/schema/types`

--- src/views/CreateForm/components/AddModal/components/types.ts
+++
@@ -1,4 +0,0 @@
-export type WidgetTypes = "field" | "ui";
-export type FieldPropTypes = "string" | "boolean" | "choice" | "number";
-export type UiPropTypes = "text" | "link" | "divider";
-export type PropTypes = UiPropTypes | FieldPropTypes;
