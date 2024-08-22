f392e89897c07270a30ffd5c139e6567f11a564f
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-08-22 15:07:54 +0330

    make `defaultValue` optional for all widgets

--- src/services/schema/types.ts
+++ src/services/schema/types.ts
@@ -26,7 +26,7 @@ export type StringFieldWidgetProps = CommonFieldWidgetsProperties & {
   multiline?: boolean;
   maxLength?: number;
   minLength?: number;
-  defaultValue: string;
+  defaultValue?: string;
 };
 
 export type StringFieldWidget = {
@@ -38,7 +38,7 @@ export type NumberFieldWidgetProps = CommonFieldWidgetsProperties & {
   placeholder?: string;
   max?: number;
   min?: number;
-  defaultValue: number;
+  defaultValue?: number;
 };
 
 export type NumberFieldWidget = {
@@ -65,7 +65,7 @@ export type ChoiceFieldWidgetProps = CommonFieldWidgetsProperties & {
   maxRequired?: number;
   shuffleOptions?: boolean;
   multiSelect: boolean;
-  defaultValue: string[] | string;
+  defaultValue?: string[] | string;
   options: ChoiceOption[];
 };
 
