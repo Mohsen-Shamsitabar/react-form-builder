0cfe1e901192c328478b52af993d072c00459f51
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-11-21 15:59:17 +0330

    refactor treeView

--- src/views/CreateForm/components/FormEditor/components/FormHierarchy/FormHierarchy.tsx
+++ src/views/CreateForm/components/FormEditor/components/FormHierarchy/FormHierarchy.tsx
@@ -7,6 +7,7 @@ import {
 import { Box, Button, Stack } from "@mui/material";
 import { TreeItem, TreeView } from "@mui/x-tree-view";
 import * as React from "react";
+import { type SchemaID } from "services/schema/types";
 import type { SystemSX } from "types";
 import { mergeSx } from "utils";
 import { useFormStateManager } from "views/CreateForm/form-state-manager";
@@ -22,41 +23,90 @@ type Props = {
 const FormHierarchy = (props: Props) => {
   const { sx: sxProps } = props;
 
-  const renderTree = React.useCallback((data: CreateFormData) => {
-    const { pages, widgets } = data;
+  const formStateManager = useFormStateManager();
 
-    const renderWidgets = (page: PageNode) => {
-      const { widgets: pageWidgets } = page;
+  const prevPagesRef = React.useRef(
+    !formStateManager ? [] : formStateManager.state.pages.allIds,
+  );
 
-      return pageWidgets?.map(widgetId => {
-        const widget = widgets.byId[widgetId]!;
+  const prevPages = prevPagesRef.current;
+
+  const currentPages = !formStateManager
+    ? []
+    : formStateManager.state.pages.allIds;
+
+  const [expandedItems, setExpandedItems] = React.useState<string[]>(
+    !formStateManager ? [] : currentPages,
+  );
+
+  //
+
+  if (currentPages.length > prevPages.length) {
+    // added a new page
+    const newPageId = currentPages.filter(
+      pageId => !prevPages.includes(pageId),
+    )[0]!;
+
+    setExpandedItems(c => [...c, newPageId]);
+  } else if (currentPages.length < prevPages.length) {
+    // removed a page
+    const deletedPageId = prevPages.filter(
+      pageId => !currentPages.includes(pageId),
+    )[0]!;
+
+    setExpandedItems(c => c.filter(pageId => pageId !== deletedPageId));
+  }
+
+  prevPagesRef.current = currentPages;
+
+  //
+
+  const renderTree = React.useCallback(
+    (data: CreateFormData) => {
+      const { pages, widgets } = data;
+
+      const renderWidgets = (page: PageNode) => {
+        const { widgets: pageWidgets } = page;
+
+        return pageWidgets?.map(widgetId => {
+          const widget = widgets.byId[widgetId]!;
+
+          return (
+            <TreeItem
+              nodeId={widget.id}
+              key={widget.id}
+              label={<Trigger key={widget.id} item={widget} />}
+            />
+          );
+        });
+      };
+
+      const makeHandleExpand = (pageId: SchemaID) => () => {
+        if (expandedItems.includes(pageId)) {
+          setExpandedItems(currnet => currnet.filter(id => pageId !== id));
+          return;
+        }
+        setExpandedItems(current => [...current, pageId]);
+      };
+
+      return pages.allIds.map(pageId => {
+        const page = pages.byId[pageId]!;
 
         return (
           <TreeItem
-            nodeId={widget.id}
-            key={widget.id}
-            label={<Trigger key={widget.id} item={widget} />}
-          />
+            onClick={makeHandleExpand(pageId)}
+            nodeId={pageId}
+            key={pageId}
+            label={<Trigger item={page} />}
+          >
+            {renderWidgets(page)}
+          </TreeItem>
         );
       });
-    };
+    },
+    [expandedItems],
+  );
 
-    return pages.allIds.map(pageId => {
-      const page = pages.byId[pageId]!;
-
-      return (
-        <TreeItem
-          nodeId={page.id}
-          key={page.id}
-          label={<Trigger item={page} />}
-        >
-          {renderWidgets(page)}
-        </TreeItem>
-      );
-    });
-  }, []);
-
-  const formStateManager = useFormStateManager();
   const modalManager = useModalManager();
   if (!modalManager) return null;
 
@@ -80,6 +130,7 @@ const FormHierarchy = (props: Props) => {
         </Button>
 
         <TreeView
+          expanded={expandedItems}
           aria-label="form-tree-view"
           defaultCollapseIcon={<ExpandMoreIcon />}
           defaultExpandIcon={<ChevronRightIcon />}
